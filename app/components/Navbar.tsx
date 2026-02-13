3c6d226cec6dab224a9c0835be6e4ff6bc06675a
jakkayy <niti2042547@gmail.com>
2026-02-13 18:03:24 +0700

    feat: create tab on navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,30 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 flex items-center justify-between border-b border-gray-200 bg-white px-10 py-3">
+    <header className="sticky top-0 z-50">
+      {/* Top bar */}
+      <div className="flex items-center justify-between bg-gray-100 px-10 py-2">
+        <Image src="/jordan.svg" alt="Jordan" width={20} height={20} />
+        <div className="flex items-center gap-1 text-xs text-black">
+          <Link href="#" className="px-2 hover:text-gray-500">
+            ค้นหาร้าน
+          </Link>
+          <span className="text-gray-300">|</span>
+          <Link href="#" className="px-2 hover:text-gray-500">
+            ความช่วยเหลือ
+          </Link>
+          <span className="text-gray-300">|</span>
+          <Link href="/register" className="px-2 hover:text-gray-500">
+            เข้าร่วมกับเรา
+          </Link>
+          <span className="text-gray-300">|</span>
+          <Link href="/login" className="px-2 hover:text-gray-500">
+            ลงชื่อเข้าใช้
+          </Link>
+        </div>
+      </div>
+
+    <nav className="flex items-center justify-between border-b border-gray-200 bg-white px-10 py-3">
       {/* Logo */}
       <Link href="/">
         <Image src="/nike.svg" alt="Nike" width={60} height={24} />
@@ -65,7 +88,7 @@ export default function Navbar() {
         </div>
 
         {/* Wishlist */}
-        <button className="p-1">
+        <Link href="/favorites" className="p-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -80,10 +103,10 @@ export default function Navbar() {
               d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
             />
           </svg>
-        </button>
+        </Link>
 
         {/* Cart */}
-        <button className="p-1">
+        <Link href="/cart" className="p-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -98,8 +121,9 @@ export default function Navbar() {
               d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
             />
           </svg>
-        </button>
+        </Link>
       </div>
     </nav>
+    </header>
   );
 }
