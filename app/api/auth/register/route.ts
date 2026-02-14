e31cad550a8ef8e6dd52bef31df086f11e17fbe4
jakkayy <niti2042547@gmail.com>
2026-02-14 20:41:02 +0700

    feat: create api register

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,51 @@
+import { signToken } from "@/app/lib/jwt";
+import { prisma } from "@/app/lib/prisma";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    const { firstname, lastname, email, password} = await req.json();
+
+    const checkEmail = await prisma.user.findUnique({
+        where: { email: email },
+    })
+
+    if (checkEmail) {
+        return NextResponse.json(
+            { error: "email has been used"},
+            { status: 400 }
+        );
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+        data: {
+            firstname: firstname,
+            lastname: lastname,
+            email: email,
+            password: hashPassword,
+        },
+        select: {
+            id: true,
+            role: true,
+        }
+    });
+
+    const token = signToken({
+        id: user.id,
+        role: user.role,
+    });
+    
+    (await cookies()).set("access_token", token, {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+    })
+
+    return NextResponse.json({
+        user: { user_id: user.id, role: user.role }
+    });
+}
