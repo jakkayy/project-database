b62348755b6f71d10b09379ce5bfa684e1f6f559
jakkayy <niti2042547@gmail.com>
2026-02-16 16:26:15 +0700

    feat: change ลงชื่อเข้าใช้ to firstname when login success

---
+++ app/api/profile/get-account/route.ts
@@ -0,0 +1,34 @@
+import { prisma } from "@/app/lib/prisma";
+import { cookies } from "next/headers";
+import { requireAuth } from "@/app/lib/auth";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const token = (await cookies()).get("access_token")?.value;
+    if (!token) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const userAuth = requireAuth(token);
+
+    const user = await prisma.user.findUnique({
+      where: { id: userAuth.user_id },
+      select: {
+        id: true,
+        firstname: true,
+        lastname: true,
+        email: true,
+        role: true,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(user);
+  } catch (error) {
+    return NextResponse.json({ message: "Invalid token" }, { status: 401 });
+  }
+}
