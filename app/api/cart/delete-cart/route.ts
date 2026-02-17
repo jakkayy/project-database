115dfd618596b0d0af4d848a58f39023db42b349
jakkayy <niti2042547@gmail.com>
2026-02-18 01:07:12 +0700

    feat: create api delete product in cart

---
+++ app/api/cart/delete-cart/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/app/lib/prisma";
+
+export async function DELETE(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const cartItem_id = Number(searchParams.get("cartItem_id"));
+
+  if (!cartItem_id) {
+    return NextResponse.json(
+      { error: "cartItem_id is required" },
+      { status: 400 }
+    );
+  }
+
+  await prisma.cartItem.delete({
+    where: { cartItem_id },
+  });
+
+  return NextResponse.json({ message: "Item removed" });
+}
+
