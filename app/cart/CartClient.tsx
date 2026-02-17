f5e75e96764b4695cf2652565ac7a4879b53093b
jakkayy <niti2042547@gmail.com>
2026-02-18 01:09:37 +0700

    feat: create CartClient to use in page cart

---
+++ app/cart/CartClient.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useState } from "react";
+import CartItem from "@/app/components/CartItem";
+import CartSummary from "@/app/components/CartSummary";
+
+interface Props {
+  items: any[];
+  total: number;
+}
+
+export default function CartClient({ items, total }: Props) {
+  const [cartItems, setCartItems] = useState(items);
+
+  const handleRemove = (id: number) => {
+    setCartItems(prev => prev.filter(item => item.cartItem_id !== id));
+  };
+
+  const newTotal = cartItems.reduce(
+    (sum, item) => sum + item.price,
+    0
+  );
+
+  console.log(items);
+
+  return (
+    <div className="mx-auto max-w-7xl px-10 py-10">
+      <div className="flex gap-12">
+        <div className="flex-1">
+          <h1 className="mb-4 text-2xl font-medium text-black">ตะกร้า</h1>
+
+          {cartItems.map((item) => (
+            <CartItem
+              key={item.cartItem_id}
+              cartItem_id={item.cartItem_id}
+              image={item.product?.images?.[0]}
+              name={item.product?.name}
+              description={item.product?.category}
+              color="-"
+              size="-"
+              price={item.price.toLocaleString("th-TH", {
+                style: "currency",
+                currency: "THB",
+              })}
+              initialQty={item.quantity}
+              onRemove={handleRemove}
+            />
+          ))}
+        </div>
+
+        <div className="w-80 shrink-0">
+          <CartSummary
+            subtotal={newTotal.toLocaleString("th-TH", {
+              style: "currency",
+              currency: "THB",
+            })}
+            total={newTotal.toLocaleString("th-TH", {
+              style: "currency",
+              currency: "THB",
+            })}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
