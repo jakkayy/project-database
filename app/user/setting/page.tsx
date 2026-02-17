cf4a78842c3668bd7022b8cf56ee30da356679f3
jakkayy <niti2042547@gmail.com>
2026-02-17 16:15:20 +0700

    refactor: move setting in folder user

---
+++ app/user/setting/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../../components/Navbar";
+import SettingContent from "../../components/SettingContent";
+
+export default function SettingPage() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      <SettingContent />
+    </div>
+  );
+}
