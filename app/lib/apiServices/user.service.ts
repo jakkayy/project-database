981adb4c3acbdb747d1f4d01e2cec7c2d78cf50b
jakkayy <niti2042547@gmail.com>
2026-02-18 01:06:29 +0700

    feat: add function connect api

--- app/lib/apiServices/user.service.ts
+++ app/lib/apiServices/user.service.ts
@@ -7,3 +7,46 @@ export function getAccount() {
 export function getAllProduct() {
     return apiFetch("/api/product/get-all-product");
 }
+
+export function addCart(data: { 
+    product_id: string, 
+    quantity: number,
+    basePrice: number,
+}) {
+    return apiFetch("/api/cart/add-cart", {
+        method: "POST",
+        body: JSON.stringify(data),
+    });
+}
+
+export function getCart() {
+    return apiFetch("/api/cart/get-cart");
+}
+
+export function deleteCart(cartItem_id: number) {
+    return apiFetch(`/api/cart/delete-cart?cartItem_id=${cartItem_id}`, {
+        method: "DELETE",
+    });
+}
+
+export function increaseCart(cartItem_id: number) {
+    return apiFetch(`/api/cart/${cartItem_id}?action=increase`, {
+        method: "PATCH",
+    });
+}
+
+export function decreaseCart(cartItem_id: number) {
+    return apiFetch(`/api/cart/${cartItem_id}?action=decrease`, {
+        method: "PATCH",
+    });
+}
+
+export function updateCartItem(
+    cartItem_id: number,
+    action: "increase" | "decrease"
+) {
+    return apiFetch(`/api/cart/${cartItem_id}`, {
+        method: "PATCH",
+        body: JSON.stringify({ action }),
+    });
+}
