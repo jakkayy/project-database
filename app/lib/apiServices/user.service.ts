2b5944ffa105eb5b28b441852b2af28101506a93
jakkayy <niti2042547@gmail.com>
2026-02-17 17:32:20 +0700

    feat: add function getAllProduct

--- app/lib/apiServices/user.service.ts
+++ app/lib/apiServices/user.service.ts
@@ -3,3 +3,7 @@ import { apiFetch } from "./client";
 export function getAccount() {
     return apiFetch("/api/profile/get-account");
 }
+
+export function getAllProduct() {
+    return apiFetch("/api/product/get-all-product");
+}
