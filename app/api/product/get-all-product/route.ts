84adfc0e411504c7fc6e2fbab7251522a17250b0
jakkayy <niti2042547@gmail.com>
2026-02-17 17:28:52 +0700

    feat: get all product show at homepage

--- app/api/product/get-all-product/route.ts
+++ app/api/product/get-all-product/route.ts
@@ -8,7 +8,20 @@ export async function GET() {
 
     const products = await Product.find();
 
-    return NextResponse.json(products);
+    const formattedProducts = products.map((product) => {
+      const p = product.toObject();
+
+      return {
+        ...p,
+        image:
+          p.image && p.image.trim() !== ""
+            ? p.image
+            : "/placeholder.png",
+      };
+    });
+
+    return NextResponse.json(formattedProducts);
+
   } catch (error) {
     return NextResponse.json(
       { error: "Failed to fetch products" },
