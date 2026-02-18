05ae208fb9f2c9850e5a8dcc9d77d57fac0ff678
jakkayy <niti2042547@gmail.com>
2026-02-18 15:36:34 +0700

    refactor: ecit structure

--- app/api/cart/[cartItem_id]/route.ts
+++ app/api/cart/[cartItem_id]/route.ts
@@ -5,9 +5,9 @@ export async function PATCH(
   req: Request,
   context: { params: Promise<{ cartItem_id: string }> }
 ) {
-  const { cartItem_id } = await context.params; 
-
+  const { cartItem_id } = await context.params;
   const id = Number(cartItem_id);
+
   const { action } = await req.json();
 
   const existing = await prisma.cartItem.findUnique({
@@ -21,14 +21,11 @@ export async function PATCH(
     );
   }
 
-  const unitPrice = Number(existing.price) / existing.quantity;
-
   if (action === "increase") {
     const updated = await prisma.cartItem.update({
       where: { cartItem_id: id },
       data: {
         quantity: { increment: 1 },
-        price: { increment: unitPrice },
       },
     });
 
@@ -48,7 +45,6 @@ export async function PATCH(
       where: { cartItem_id: id },
       data: {
         quantity: { decrement: 1 },
-        price: { decrement: unitPrice },
       },
     });
 
