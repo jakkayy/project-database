ad673298c64f7c6eab9342545e901bb514b9dbfd
jakkayy <niti2042547@gmail.com>
2026-03-01 12:12:11 +0700

    add config hostname

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "static.nike.com",
-        pathname: "/**",
-      },
+      { protocol: "https", hostname: "static.nike.com", pathname: "/**" },
+      { protocol: "https", hostname: "media.arirunningstore.com", pathname: "/**" },
+      { protocol: "https", hostname: "www.footlocker.co.th", pathname: "/**" },
+      { protocol: "https", hostname: "www.converse.co.th", pathname: "/**" },
+      { protocol: "https", hostname: "run2paradise.com", pathname: "/**" },
+      { protocol: "https", hostname: "www.nicekicks.com", pathname: "/**" },
+      { protocol: "https", hostname: "vsathletics.com", pathname: "/**" },
+      { protocol: "https", hostname: "www.central.co.th", pathname: "/**" },
+      { protocol: "https", hostname: "underarmour.scene7.com", pathname: "/**" },
+      { protocol: "https", hostname: "image.goxip.com", pathname: "/**" },
+      { protocol: "https", hostname: "images.stockx.com", pathname: "/**" },
     ],
   },
 };
