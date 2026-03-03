6b23e09daf9c02db08b78a485f1da10a0d7e0e5c
Natrius <natephutare@gmail.com>
2026-03-03 16:47:18 +0700

    migration(mongo_product): store reviews with rating and comment instead and create averageRating virtual

--- app/models/Product.ts
+++ app/models/Product.ts
@@ -8,10 +8,43 @@ const productSchema = new mongoose.Schema(
     basePrice: Number,
     images: [String],
     tags: [String],
-    rating: Number,
+
+    reviews: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        rating: {
+          type: Number,
+          required: true,
+          min: 1,
+          max: 5,
+        },
+        comment: String,
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },  
+    toObject: { virtuals: true },
+  }
 );
+
+productSchema.virtual("averageRating").get(function () {
+  if (!this.reviews || this.reviews.length === 0) return 0;
+
+  return (
+    this.reviews.reduce((sum, r) => sum + r.rating, 0) /
+    this.reviews.length
+  );
+});
 
 export default mongoose.models.Product ||
   mongoose.model("Product", productSchema);
